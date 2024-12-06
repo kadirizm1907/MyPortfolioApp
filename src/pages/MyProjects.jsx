3b05ca9bf0336f8d64dabe3eade5dcd83171544a
kadirizm <kadiryurt1907@gmail.com>
2024-12-06 22:12:49 +0100

    h1 maded dynemic

--- src/pages/MyProjects.jsx
+++ src/pages/MyProjects.jsx
@@ -1,31 +1,52 @@
-
-import { Col, Row } from 'react-bootstrap'
-import { Outlet, useNavigate } from 'react-router-dom'
+import { useState } from "react";
+import { Row, Col } from "react-bootstrap";
+import { useNavigate, Outlet } from "react-router-dom";
 
 const MyProjects = () => {
-    const navigate = useNavigate()
-    return (
-        <div className="container text-center mt-5">
-            <div className='mt-5'>
-                <h1>SOME OF MY PROJECTS</h1>
-            </div>
-            <Row className='container cursor text-center mt-4'>
-                <Col lg={4} md={4} sm={12}>
-                <div id='splash' className=' pointer-event pe-auto mt-2' onClick={() => navigate("js")}>JS</div>
-                </Col>
-                <Col lg={4} md={4} sm={12}>
-                <div id='splash2' className=' mt-2' onClick={() => navigate("react")}>REACT</div>
-                </Col>
-                <Col lg={4} md={4} sm={12}>
+  const navigate = useNavigate();
+  const [projectType, setProjectType] = useState("MY"); // Varsayılan metin
 
-                <div id='splash3' className=' mt-2' onClick={() => navigate("html")}>HTML-CSS</div>
-                </Col>
-                
-            </Row>
+  const handleNavigation = (type, path) => {
+    setProjectType(type); // Proje türünü güncelle
+    navigate(path); // Yönlendirme yap
+  };
 
-            < Outlet />
-        </div>
-    )
-}
+  return (
+    <div className="container text-center mt-5">
+      <div className="mt-5">
+        <h1>SOME OF {projectType} PROJECTS</h1>
+      </div>
+      <Row className="container cursor text-center mt-4">
+        <Col lg={4} md={4} sm={12}>
+          <div
+            id="splash"
+            className="pointer-event pe-auto mt-2"
+            onClick={() => handleNavigation("JS", "js")}
+          >
+            JS
+          </div>
+        </Col>
+        <Col lg={4} md={4} sm={12}>
+          <div
+            id="splash2"
+            className="mt-2"
+            onClick={() => handleNavigation("REACT", "react")}
+          >
+            REACT
+          </div>
+        </Col>
+        <Col lg={4} md={4} sm={12}>
+          <div
+            id="splash3"
+            className="mt-2"
+            onClick={() => handleNavigation("HTML-CSS", "html")}
+          >
+            HTML-CSS
+          </div>
+        </Col>
+      </Row>
+      < Outlet />    </div>
+  );
+};
 
-export default MyProjects
+export default MyProjects;
