aa3885591089d41d4ee5a62a95a20463b68ee811
kadirizm <kadiryurt1907@gmail.com>
2024-12-15 23:15:04 +0100

    VERCEL DEPLOYED

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
+<SpeedInsights/>
